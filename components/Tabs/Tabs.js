09ad6ec19f747c4f71ef9b7ad4e18e8292ba04a5
PetejStone <petejstoney@gmail.com>
2019-04-11 14:19:23 -0700

    got remove method working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -17,18 +17,22 @@ class TabLink {
      this.tabItem = new TabItem(this.itemElement);
 
     // Add a click event listener on this instance, calling the select method on click
-
+     this.link.addEventListener('click', this.select.bind(this));
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+     const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+     Array.from(links).forEach(link => {
+       const shown = this.link.class;
+        link.classList.remove('tabs-link-selected');
+      // console.log('testing')
+     });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    // this.link.classList.toggle('tabs-link-selected');
 
     // Call the select method on the item associated with this link
 
@@ -64,4 +68,4 @@ class TabItem {
 
 */
 const items = document.querySelectorAll('.tabs-item').forEach(item => {const leedle = new TabItem(item)});
-const links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
+let links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
