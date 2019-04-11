4eb0526316ccdb61521cc02c71e0c9af3fa16973
PetejStone <petejstoney@gmail.com>
2019-04-11 13:51:41 -0700

    got data references working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,16 +3,18 @@ class TabLink {
   constructor(link) {
     // Assign this.element to the passed in DOM element
      this.link = link;
-    
-    // Get the custom data attribute on the Link
-      this.data = this.link.dataset.tab;
 
+    // Get the custom data attribute on the Link
+     this.data = this.link.dataset.tab;
+    //console.log(this.data);
 
     // Using the custom data attribute get the associated Item element
-    //  this.itemElement = ;
+
+     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    // console.log(this.itemElement)
 
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+     this.tabItem = new TabItem(this.itemElement);
 
     // Add a click event listener on this instance, calling the select method on click
 
@@ -34,20 +36,21 @@ class TabLink {
 }
 
 class TabItem {
-  constructor(item) {
+  constructor(data) {
     // Assign this.element to the passed in element
-     this.item = item;
-     console.log(item);
+     this.element = data;
+    // console.log(this.element);
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
+    items.classList.remove('tabs-item-selected');
 
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.item.classList.toggle('tabs-item-selected');
   }
 }
 
@@ -60,5 +63,5 @@ class TabItem {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
 
 */
-const items = document.querySelectorAll('.tabs-item').forEach(item => new TabItem(item));
+const items = document.querySelectorAll('.tabs-item').forEach(item => {const leedle = new TabItem(item)});
 const links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
