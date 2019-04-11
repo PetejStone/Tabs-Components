d2ceadf335fd698e7fce75d76e9259269c43ef2c
PetejStone <petejstoney@gmail.com>
2019-04-11 14:34:33 -0700

    got second select method working

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,16 +26,16 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
      Array.from(links).forEach(link => {
-       const shown = this.link.class;
+
         link.classList.remove('tabs-link-selected');
       // console.log('testing')
      });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.link.classList.toggle('tabs-link-selected');
+     this.link.classList.toggle('tabs-link-selected');
 
     // Call the select method on the item associated with this link
-
+     this.tabItem.select();
   }
 }
 
@@ -48,13 +48,15 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    const items = document.querySelectorAll('.tabs-item');
+    const items = Array.from(document.querySelectorAll('.tabs-item'));
 
     // Remove the class "tabs-item-selected" from each element
-    items.classList.remove('tabs-item-selected');
+     items.forEach(item => {
+       item.classList.remove('tabs-item-selected');
+     })
 
     // Add a class named "tabs-item-selected" to this element
-    this.item.classList.toggle('tabs-item-selected');
+     this.element.classList.toggle('tabs-item-selected');
   }
 }
 
